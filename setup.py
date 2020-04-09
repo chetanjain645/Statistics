304707c154489025c963c20dae7a2ef4880daa48
chetan jain <43923253+chetanjain645@users.noreply.github.com>
2020-04-09 21:59:23 +0530

    Guassain and binomial distribution is ready to use in your program

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='distributions',
+      version=0.1,
+      description='Guassian distributions',
+      author = 'Chetan jain' ,
+      author_email = 'chetanjain645@yahoo.com',
+      license ='MIT',
+      packages=['distributions'],
+      zip_safe=False)
